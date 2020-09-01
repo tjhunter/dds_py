144a3f34029f67937b409b6c372efa893d58a6da
Tim Hunter <tim.hunter@nl.abnamro.com>
2020-09-01 19:02:32 +0200

    working sklearn example, with some hacks

--- dds/codecs/builtins.py
+++ dds/codecs/builtins.py
@@ -1,6 +1,8 @@
 """
 The string protocol.
 """
+import pickle
+import sklearn.model_selection._search
 
 from typing import Any
 
@@ -23,4 +25,16 @@ class StringLocalCodec(CodecProtocol):
             return f.read().decode("utf-8")
 
 
-# TODO: generic python codec based on pickles
+class PickleLocalCodec(CodecProtocol):
+
+    def ref(self): return ProtocolRef("builtins.pickle")
+
+    def handled_types(self): return [object, sklearn.model_selection._search.GridSearchCV]
+
+    def serialize_into(self, blob: Any, loc: GenericLocation):
+        with open(loc, "wb") as f:
+            pickle.dump(blob, f)
+
+    def deserialize_from(self, loc: GenericLocation) -> str:
+        with open(loc, "rb") as f:
+            return pickle.load(f)
---
+++ setup.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python
+
+#
+# Copyright (C) 2019 Databricks, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+from __future__ import print_function
+
+from io import open
+from setuptools import setup
+from os import path
+
+DESCRIPTION = "PyDDS: data-driven programing"
+
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    LONG_DESCRIPTION = f.read()
+
+# try:
+#     exec(open('dds/version.py').read())
+# except IOError:
+#     print("Failed to load Koalas version file for packaging. You must be in Koalas root dir.",
+#           file=sys.stderr)
+#     sys.exit(-1)
+# VERSION = __version__  # noqa
+VERSION = "0.1.0"
+
+setup(
+    name='dds_py',
+    version=VERSION,
+    packages=['dds'],
+    extras_require={
+        'pandas': ['pandas>=0.23.1', 'pyarrow>=0.10'],
+        'spark': ['pyspark>=2.4.0']
+    },
+    python_requires='>=3.5,<3.9',
+    install_requires=[
+    ],
+    author="Tim Hunter",
+    author_email="tjhunter+dds@cs.stanford.edu",
+    license='http://www.apache.org/licenses/LICENSE-2.0',
+    url="https://github.com/tjhunter/dds_py",
+    project_urls={
+        'Bug Tracker': 'https://github.com/tjhunter/dds_py/issues',
+        # 'Documentation': 'https://koalas.readthedocs.io/',
+        'Source Code': 'https://github.com/tjhunter/dds_py'
+    },
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type='text/markdown',
+    classifiers=[
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+    ],
+)
