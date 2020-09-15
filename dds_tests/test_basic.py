23a1f965f244c234ae65ddad8cf61e15a6373388
Tim Hunter <tim.hunter@nl.abnamro.com>
2020-09-15 08:42:06 +0200

    enabling all tests

--- dds_tests/test_basic.py
+++ dds_tests/test_basic.py
@@ -6,30 +6,30 @@ from . import utils as u
 path_1 = "/path_1"
 
 
-# def f1(i: int) -> int: return i
-#
-#
-# def f2() -> str: return "A"
-#
-#
-# def f2_wrap(): return dds.keep(path_1, f2)
-#
-#
-# @pytest.mark.usefixtures("cleandir")
-# def test_1():
-#     assert dds.eval(f2) == "A"
-#
-#
-# @pytest.mark.usefixtures("cleandir")
-# def test_2():
-#     assert dds.eval(f2_wrap) == "A"
-#
-# def fun_3(i):
-#     return "A" + str(i)
-#
-# @pytest.mark.usefixtures("cleandir")
-# def test_3():
-#     assert dds.eval(fun_3, 3) == "A3"
+def f1(i: int) -> int: return i
+
+
+def f2() -> str: return "A"
+
+
+def f2_wrap(): return dds.keep(path_1, f2)
+
+
+@pytest.mark.usefixtures("cleandir")
+def test_1():
+    assert dds.eval(f2) == "A"
+
+
+@pytest.mark.usefixtures("cleandir")
+def test_2():
+    assert dds.eval(f2_wrap) == "A"
+
+def fun_3(i):
+    return "A" + str(i)
+
+@pytest.mark.usefixtures("cleandir")
+def test_3():
+    assert dds.eval(fun_3, 3) == "A3"
 
 
 def f4(i, j=0):
--- dds_tests/test_refs.py
+++ dds_tests/test_refs.py
@@ -1,72 +1,72 @@
-# """
-# Tests for the outer references
-# """
-#
-# import dds
-# import pytest
-# from .utils import cleandir, Counter
-# from io import UnsupportedOperation
-#
-# _ = cleandir
-#
-#
-# p = "/path"
-#
-#
-# fun_1_obj = UnsupportedOperation()
-#
-#
-# def fun_1_f():
-#     _ = fun_1_obj.args
-#     return None
-#
-#
-# def fun_1_f1(): dds.keep(p, fun_1_f)
-#
-#
-# @pytest.mark.usefixtures("cleandir")
-# def test_1():
-#     """ Unauthorized objects do not trigger errors """
-#     assert dds.eval(fun_1_f1) is None
-#
-#
-# fun_2_obj = 0
-# fun_2_counter = Counter()
-#
-# def fun_2_f():
-#     _ = fun_2_obj
-#     fun_2_counter.increment()
-#     return None
-#
-# def fun_2_f1(): dds.keep(p, fun_2_f)
-#
-# @pytest.mark.usefixtures("cleandir")
-# def test_2():
-#     """ Authorized objects are taken into account """
-#     global fun_2_obj
-#     assert dds.eval(fun_2_f1) is None
-#     fun_2_obj = 1
-#     assert dds.eval(fun_2_f1) is None
-#     assert fun_2_counter.value == 2, fun_2_counter.value
-#
-#
-# fun_3_obj = UnsupportedOperation("a")
-# fun_3_counter = Counter()
-#
-# def fun_3_f():
-#     _ = fun_3_obj
-#     fun_3_counter.increment()
-#
-# def fun_3_f1(): dds.keep(p, fun_3_f)
-#
-# @pytest.mark.usefixtures("cleandir")
-# def test_3():
-#     """ Unauthorized objects are not taken into account """
-#     global fun_3_obj
-#     assert dds.eval(fun_3_f1) is None
-#     fun_3_obj = UnsupportedOperation("b")
-#     assert dds.eval(fun_3_f1) is None
-#     assert fun_3_counter.value == 1, fun_3_counter.value
-#
-#
-#
+"""
+Tests for the outer references
+"""
+
+import dds
+import pytest
+from .utils import cleandir, Counter
+from io import UnsupportedOperation
+
+_ = cleandir
+
+
+p = "/path"
+
+
+fun_1_obj = UnsupportedOperation()
+
+
+def fun_1_f():
+    _ = fun_1_obj.args
+    return None
+
+
+def fun_1_f1(): dds.keep(p, fun_1_f)
+
+
+@pytest.mark.usefixtures("cleandir")
+def test_1():
+    """ Unauthorized objects do not trigger errors """
+    assert dds.eval(fun_1_f1) is None
+
+
+fun_2_obj = 0
+fun_2_counter = Counter()
+
+def fun_2_f():
+    _ = fun_2_obj
+    fun_2_counter.increment()
+    return None
+
+def fun_2_f1(): dds.keep(p, fun_2_f)
+
+@pytest.mark.usefixtures("cleandir")
+def test_2():
+    """ Authorized objects are taken into account """
+    global fun_2_obj
+    assert dds.eval(fun_2_f1) is None
+    fun_2_obj = 1
+    assert dds.eval(fun_2_f1) is None
+    assert fun_2_counter.value == 2, fun_2_counter.value
+
+
+fun_3_obj = UnsupportedOperation("a")
+fun_3_counter = Counter()
+
+def fun_3_f():
+    _ = fun_3_obj
+    fun_3_counter.increment()
+
+def fun_3_f1(): dds.keep(p, fun_3_f)
+
+@pytest.mark.usefixtures("cleandir")
+def test_3():
+    """ Unauthorized objects are not taken into account """
+    global fun_3_obj
+    assert dds.eval(fun_3_f1) is None
+    fun_3_obj = UnsupportedOperation("b")
+    assert dds.eval(fun_3_f1) is None
+    assert fun_3_counter.value == 1, fun_3_counter.value
+
+
+
