8187a198b514818e31bd8c8592d35758964e566c
Tim Hunter <tjhunter@cs.stanford.edu>
2020-09-23 08:45:09 +0200

    NameConstant

--- dds/fun_args.py
+++ dds/fun_args.py
@@ -97,7 +97,8 @@ def get_arg_ctx_ast(
         else:
             # Get the AST node:
             node = args[idx]
-            if isinstance(node, ast.Constant):
+            # NameConstant for python 3.5 - 3.7
+            if isinstance(node, (ast.Constant, ast.NameConstant)):
                 # We can deal with some constant nodes
                 default_ob = node.value if node.value is not None else "__none__"
                 h = dds_hash(default_ob)
