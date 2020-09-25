f92fd0b114ded4b3ccf3edb793e9e0139127fe1e
Timothy Hunter <tjhunter@eecs.berkeley.edu>
2020-09-25 10:06:34 +0200

    more docs (#18)
    Co-authored-by: Tim Hunter <tjhunter@cs.stanford.edu>

--- README.md
+++ README.md
@@ -1,2 +1,51 @@
-# dds_py
+# dds_py - Data driven software
+
 Data-driven software (python implementation)
+
+## Introduction
+
+The DDS package solves the synchronization problem between code and data. It allows programmers,
+ scientists and data scientists to integrate code with data and data with code without fear of
+ stale data, disparate storage frameworks or concurrency issues. DDS allows quick collaboration and 
+ data software reuse without the complexity.
+ 
+In the world of data-driven software, executing code leads to the creation of _data artifacts_, which can be 
+of any sort and shape that the work requires:
+- _datasets_ : collections of data items
+- _models_ : compact representations of datasets for specific tasks (classification, ...)
+- _insights_: information about datasets and models that provide human-relatable cues about other artifacts
+
+Combining software with data is currently a hard challenge, because existing programming paradigms
+aim at being universal and are not tuned to the specific challenges of combining data and code 
+within a single product. DDS provides the low-level foundations to do that, in the spirit
+of Karparthy's Software 2.0 directions (TODO: cite). `dds_py` is a software implementation of these ideas
+
+Here is the Hello world example (using type annotations for clarity)
+
+```python
+import dds
+import requests 
+
+def data_creator() -> str:
+  url = "https://gist.githubusercontent.com/bigsnarfdude/515849391ad37fe593997fe0db98afaa/raw/f663366d17b7d05de61a145bbce7b2b961b3b07f/weather.csv"
+  return requests.get(url=url, verify=False).content.decode("utf-8")
+
+data: str = dds.keep("/hello_data", data_creator)
+```
+This example does the following:
+- it defines a source of data, here a piece of weather data from the internet. This source is defined as the function `data_creator`
+- it assigns the data produced by this source into a variable (`data`) and also to a path in a storage system (`/hello_data`) 
+
+The DDS library guarantees the following after evaluation of the code:
+1. the path `/hello_data` contains a copy of the data returned by `data_creator`, as if the function `data_creator` had been called at this moment
+2. the function `data_creator` is only evaluated when its inputs, or its code, are modified (referential transparency)
+
+This model has profound consequences for the programmers:
+- computationally expensive data functions (such as building models) can be composed and built upon _very cheaply_, as if they were
+variables. DDS alleviates the need to decompose data pipelines into multiple stages because of technological requirments. 
+
+At its core, the programming model of DDS is very simple:
+- functions are assumed to be idempotent, if not pure
+- functions are referentially transparent (they can be replaced with their output)
+- artifacts of any sort (models, data, statistics) are stored in a central repository
+- the programming model is assumed to be hermetic (only the I/O tracked within the framework is expected to happen)
--- dds/__init__.py
+++ dds/__init__.py
@@ -28,12 +28,119 @@ def keep(
     *args: Tuple[Any, ...],
     **kwargs: Dict[str, Any]
 ) -> _Out:
+    """
+    Stores the result of calling a function to a specific path. If this particular evaluation has not happened before,
+    the function will be evaluated again (with the given arguments).
+
+    For example, instead of writing:
+
+    >>> data = my_function(arg1, arg2)
+
+    you should use:
+
+    >>> data = dds.keep(path, my_function, arg1, arg2)
+
+    Accepted functions.
+    -------------------
+
+    In general, the functions that should be provided are total, non-recursive, deterministic and referentially
+     transparent.
+
+    Functions have currently the following restrictions:
+    - no static method, class method
+    - not recursive
+    - no generators
+    - the functions must be in a whitelisted module to be considered, see the whitelist_module() function
+
+    They must return storable objects. The exact list depends on the store that is currently deployed.
+
+    Accepted arguments.
+    -------------------
+
+    Only the following classes of arguments are accepted:
+    - the basic types of python (int, str, bool, float)
+    - lists and tuples of accepted arguments
+    - dictionaries. They are evaluated as sorted lists (by their keys)
+
+    Using complex arguments
+    -----------------------
+
+    If more complex arguments should be accepted, two strategies are possible:
+    - embed them inside the function call
+    - wrap them inside a function which is then called through eval()
+
+    Example: the following code will fail:
+
+    >>> df = pd.DataFrame({"x":[1]})
+    >>>
+    >>> def my_stats(data: pd.DataFrame) -> int: return len(data)
+    >>>
+    >>> stats = dds.keep("/stats", my_stats, data)
+
+    The workaround is to use a wrapper function that create the dataframe:
+
+    >>> def my_pipeline():
+    >>>     df = pd.DataFrame({"x":[1]})
+    >>>     return dds.keep("/stats", my_stats, df) # This will work
+    >>>
+    >>> stats = dds.eval(my_pipeline)
+
+    Another possibility is to move keep the result of the pipeline instead:
+
+    >>> def my_pipeline2():
+    >>>     df = pd.DataFrame({"x":[1]})
+    >>>     return my_stats(df)
+    >>>
+    >>> stats = dds.keep("/stats", my_pipeline2)
+
+    The difference is that my_pipeline will be evaluated at each call (but not my_stats), while my_pipeline2 will
+    only be evaluated once.
+
+    :param path: a path in the storage system which will store the content of the function, evaluated to the given
+    arguments. It is expected to be in absolute form (starting with "/" if a string
+    or being an absolute path if a pathlib's Path object).
+    If this path exists, it will be overwritten silently.
+
+    :param fun: A function to evaluate. See text above on the limitations over this function
+    :param args: the arguments of this function
+    :param kwargs: (keyworded arguments are currently unsupported)
+    :return: the value that the function would produce for these arguments
+    """
     return _keep(path, fun, *args, **kwargs)
 
 
 def eval(
     fun: Callable[[_In], _Out], *args: Tuple[Any, ...], **kwargs: Dict[str, Any]
 ) -> _Out:
+    """
+    Evaluates a function. The result of the function is not stored in the data store, but the function itself may
+    contain multiple calls to keep().
+
+    This function is useful to consider in one evaluation multiple functions that may themselves call keep() or
+    refer to each other. eval() allows keep() calls to refer to complex arguments that cannot be evaluated before
+    runtime. eval() als ensures some basic rules such as no circular references, and will eventually
+    enable automatic parallel execution of internal statements.
+
+    See also the documentation of keep() for an example of eval().
+
+    Simple example.
+
+    >>> def f1_internal(): return 1
+    >>>
+    >>> def f1(): return dds.keep("/f1_value", f1_internal)
+    >>>
+    >>> def f2(): return 2 + f1()
+    >>>
+    >>> def all_fs():
+    >>>     f1()
+    >>>     dds.keep("/f2_value", f2)
+    In this example, the function is evaluated only once and its result is served to f1 and f2.
+
+    :param fun: a function. See eval() for restrictions.
+    :param args: arguments. See eval() for restrictions.
+    :param kwargs: (keyworded arguments are currently unsupported)
+    :return: the return value of the function.
+    """
     return _eval(fun, *args, **kwargs)
 
 
@@ -43,8 +150,39 @@ def set_store(
     data_dir: Optional[str] = None,
     dbutils: Optional[Any] = None,
 ) -> None:
+    """
+    Sets a new store.
+
+    By default, a local store pointing to /tmp/ is created.
+    TODO: change this to the user directory.
+
+    :param store: a type of store. Currently 'local' (local hard drive) or 'dbfs' (databricks environment) are supported
+    :param internal_dir: a path in a filesystem for the internal storage. The internal storage contains evaluated blobs
+    and corresponding metadata.
+        - for local: a path in the local filesystem
+        - for dbfs: a path in DBFS
+    :param data_dir: a path in a filesystem for the data storage. All paths provided by the user are guaranteed to map
+    to a path in the given data storage.
+        - for local: a path in the local filesystem. It contains symbolic links to the internal storage
+        - for dbfs: a path in DBFS. Objects are copied from the internal storage to the data storage
+    :param dbutils: (required only for the 'dbfs' store) the dbutils object that is in a notebook
+    :return: nothing
+    """
     _set_store(store, internal_dir, data_dir, dbutils)
 
 
 def whitelist_module(module: Union[str, ModuleType]) -> None:
+    """
+    Marks a module as whitelisted for introspection.
+
+    Example to ensure that all the functions in my_lib are considered by DDS.
+
+    >>> import my_lib
+    >>> dds.whitelist_module(my_lib)
+
+    alternative approach if you do not want to import the my_lib module:
+
+    >>> dds.whitelist_module("my_lib")
+
+    """
     return _whitelist_module(module)
