946fb4ceff1fa1a02ce6ae0c9031e98c532d879a
Timothy Hunter <tjhunter@eecs.berkeley.edu>
2020-09-30 16:26:58 +0200

    BinOp fix (#33)
    * tests
    
    * working test
    
    * working test
    
    Co-authored-by: Tim Hunter <tjhunter@cs.stanford.edu>

---
+++ dds_tests/test_binop.py
@@ -0,0 +1,32 @@
+import dds
+import pytest
+from .utils import cleandir, Counter, spath
+
+_ = cleandir
+
+cst = 3
+
+
+def z():
+    return 4
+
+
+def _fun():
+    x = (z() * 3).real("")  # type:ignore
+    return x
+
+
+def fun():
+    return dds.keep("/path", _fun) * 2
+
+
+def f():
+    fun() * 6
+
+
+@pytest.mark.usefixtures("cleandir")
+def test():
+    try:
+        fun()
+    except TypeError:
+        pass
