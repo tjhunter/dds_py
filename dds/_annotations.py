b252111bbb1837eec07f2b849da75af29eb2d099
Tim Hunter <tjhunter@cs.stanford.edu>
2020-10-01 17:53:50 +0200

    cleanup

--- dds/_annotations.py
+++ dds/_annotations.py
@@ -1,6 +1,6 @@
 import functools
 import pathlib
-from typing import Any, Callable, TypeVar, cast
+from typing import Any, Callable, TypeVar, cast, overload, Type
 from typing import Union
 
 from ._api import keep as _keep
@@ -9,13 +9,22 @@ from .structures import DDSPath
 F = TypeVar("F", bound=Callable[..., Any])
 FF = TypeVar("FF", bound=Callable[..., Callable[..., Any]])
 
+T_CallableOrType = TypeVar("T_CallableOrType", Callable[..., Any], Type[Any])
 
-def dds_function(path: Union[str, DDSPath, pathlib.Path]) -> FF:
-    def decorator(func: F) -> F:
+# @overload
+# def dds_function(path: Union[str, DDSPath, pathlib.Path]) -> Callable[[T_CallableOrType], T_CallableOrType]:
+#     ...
+
+# @overload
+def dds_function(
+    path: Union[str, DDSPath, pathlib.Path]
+) -> Callable[[T_CallableOrType], T_CallableOrType]:
+    def decorator_(func: F) -> F:
         @functools.wraps(func)
         def wrapper(*args, **kwargs):
             return _keep(path, func, *args, **kwargs)
 
         return cast(F, wrapper)
 
-    return cast(FF, decorator)
+    return decorator_
+    # return cast(FF, decorator_)
