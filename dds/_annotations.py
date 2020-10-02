50b0cc171b7a1dfcba31cdd578d8378bc92b1c87
Tim Hunter <tjhunter@cs.stanford.edu>
2020-10-02 10:05:47 +0200

    plot fix

--- dds/_annotations.py
+++ dds/_annotations.py
@@ -1,21 +1,14 @@
 import functools
 import pathlib
-from typing import Any, Callable, TypeVar, cast, overload, Type
-from typing import Union
+from typing import Any, Callable, TypeVar, cast, Type, Union
 
 from ._api import keep as _keep
 from .structures import DDSPath
 
 F = TypeVar("F", bound=Callable[..., Any])
-FF = TypeVar("FF", bound=Callable[..., Callable[..., Any]])
-
 T_CallableOrType = TypeVar("T_CallableOrType", Callable[..., Any], Type[Any])
 
-# @overload
-# def dds_function(path: Union[str, DDSPath, pathlib.Path]) -> Callable[[T_CallableOrType], T_CallableOrType]:
-#     ...
 
-# @overload
 def dds_function(
     path: Union[str, DDSPath, pathlib.Path]
 ) -> Callable[[T_CallableOrType], T_CallableOrType]:
@@ -27,4 +20,3 @@ def dds_function(
         return cast(F, wrapper)
 
     return decorator_
-    # return cast(FF, decorator_)
