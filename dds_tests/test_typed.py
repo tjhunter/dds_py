9b515d47f5ea2d61f083c3ffb64f1604e80f5270
Tim Hunter <tjhunter@cs.stanford.edu>
2020-12-18 13:21:08 +0100

    test fix

--- dds_tests/test_typed.py
+++ dds_tests/test_typed.py
@@ -1,11 +1,16 @@
 import dds
+import pytest
+from .utils import cleandir
+
+_ = cleandir
+
 
 
 @dds.dds_function("/test")
 def f() -> str:
     return ""
 
-
+@pytest.mark.usefixtures("cleandir")
 def test_typed_annotation():
-    x = f()
-    z: int = x
+    x: str = f()
+    _ = x
