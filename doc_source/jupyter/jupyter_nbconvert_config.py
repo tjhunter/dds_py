aaabf612d565e00963e8e8fe66b1999461841882
Timothy Hunter <tjhunter@eecs.berkeley.edu>
2020-12-30 10:31:04 +0100

    changes

---
+++ doc_source/jupyter/jupyter_nbconvert_config.py
@@ -0,0 +1,4 @@
+c.ClearMetadataPreprocessor.enabled=False
+c.TagRemovePreprocessor.enabled=True
+c.TagRemovePreprocessor.remove_cell_tags=set(["hide_cell"])
+# c.ClearOutputPreprocessor.enabled=True
