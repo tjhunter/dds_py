768fe4a9e4e18868069e2e909e3f22d3cd5e4b74
Timothy Hunter <tjhunter@eecs.berkeley.edu>
2021-01-06 17:47:41 +0100

    removing pandas

--- dds/codecs/pandas.py
+++ dds/codecs/pandas.py
@@ -16,26 +16,6 @@ from ..structures import (
 
 _logger = logging.getLogger(__name__)
 
-#
-# class PandasLocalCodec(CodecProtocol):
-#     def ref(self):
-#         return ProtocolRef("default.pandas_local")
-#
-#     def handled_types(self):
-#         return [ST("pandas.DataFrame"), ST("pandas.core.frame.DataFrame")]
-#
-#     def serialize_into(self, blob: Any, loc: GenericLocation):
-#         import pandas
-#
-#         assert isinstance(blob, pandas.DataFrame)
-#         blob.to_parquet(loc)
-#         _logger.debug(f"Committed dataframe to parquet: {loc}")
-#
-#     def deserialize_from(self, loc: GenericLocation) -> "pandas.DataFrame":
-#         import pandas
-#
-#         return pandas.read_parquet(loc)
-
 
 class PandasFileCodec(FileCodecProtocol):
     def ref(self):
